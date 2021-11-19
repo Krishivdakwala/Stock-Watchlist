560d076a5d9b83f385fef21f7b58c63d90a348d9
Akash-213 <akash213kulkarni@gmail.com>
2021-11-20 00:02:39 +0530

    authentication modified

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 require("./db/mongoose");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 const userRoutes = require("./routes/userRoutes");
 const watchlistRoutes = require("./routes/watchlistRoutes");
 const alertRoutes = require("./routes/alertRoutes");
+const usersRoutes = require("./routes/usersRoutes");
 
 app.use("/user", userRoutes);
 app.use("/watchlist", watchlistRoutes);
 app.use("/alert", alertRoutes);
+
+app.use("/users", usersRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("API Working");
---
+++ server/src/routes/usersRoutes.js
@@ -0,0 +1,16 @@
+const {
+  authUser,
+  registerUser,
+  updateUserProfile,
+} = require("../controllers/userControllers");
+
+// const { protect } = require("../middleware/authMiddleware");
+
+const express = require("express");
+const router = express.Router();
+
+router.route("/register").post(registerUser);
+router.post("/login", authUser);
+// router.route("/profile").post(protect, updateUserProfile);
+
+module.exports = router;
