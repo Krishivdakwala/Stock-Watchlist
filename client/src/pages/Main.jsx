4008ff881eebf570db4a1c93dcf852463cfb1a3c
Akash-213 <akash213kulkarni@gmail.com>
2021-11-20 12:43:55 +0530

    mainscreen added

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 
 import { NavLink, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import MainScreen from "../components/MainScreen";
 
 const Main = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin);
@@ -15,13 +16,15 @@ const Main = ({ history }) => {
   }, [history, userInfo]);
   return (
     <div>
-      <h2>This is landing screen</h2>
-      <NavLink to={"/register"}>
-        <Button variant="primary">Register</Button>
-      </NavLink>
-      <NavLink to={"/login"}>
-        <Button variant="primary">Log In</Button>
-      </NavLink>
+      <MainScreen>
+        <h2>This is landing screen</h2>
+        <NavLink to={"/register"}>
+          <Button variant="primary">Register</Button>
+        </NavLink>
+        <NavLink to={"/login"}>
+          <Button variant="primary">Log In</Button>
+        </NavLink>
+      </MainScreen>
     </div>
   );
 };
