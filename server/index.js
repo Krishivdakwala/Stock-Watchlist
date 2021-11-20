d4dc516d9d7b3138335a06c0716f43edad24f295
Akash-213 <akash213kulkarni@gmail.com>
2021-11-20 15:13:35 +0530

    mongodb cluster added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const app = require("./src/app");
+const connectDB = require("./src/db/mongoose");
 
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/stock-watchlist", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const dbname = "Stocks-DB";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `mongodb+srv://stock-watchlist:stock-watchlist@cluster0.5wmyu.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
