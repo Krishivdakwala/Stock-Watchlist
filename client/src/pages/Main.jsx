2d4e5a9424080816d8dd2d04559410cb57e8016d
Krishivdakwala <krishivdakwala@yahoo.com>
2021-11-20 19:07:55 +0530

    Changes in Watchlist

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
-
+import {
+  HeaderContainer,
+  HeaderBG,
+  VideoBG,
+  HeaderContent,
+  HeaderH1,
+  WhiteBtn,
+  GreenBtn,
+  HeaderBtn,
+} from "../components/LandingElements";
+import Video from "./video.mp4";
 import { NavLink, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import MainScreen from "../components/MainScreen";
@@ -15,8 +25,8 @@ const Main = ({ history }) => {
     }
   }, [history, userInfo]);
   return (
-    <div>
-      <MainScreen>
+    <>
+      {/* <MainScreen>
         <h2>This is landing screen</h2>
         <NavLink to={"/register"}>
           <Button variant="primary">Register</Button>
@@ -24,8 +34,16 @@ const Main = ({ history }) => {
         <NavLink to={"/login"}>
           <Button variant="primary">Log In</Button>
         </NavLink>
-      </MainScreen>
-    </div>
+      </MainScreen> */}
+      <HeaderContainer>
+        <HeaderBG>
+          <VideoBG autoPlay loop muted src={Video} type="video/mp4" />
+        </HeaderBG>
+        <HeaderContent>
+          <HeaderH1>Stock Watchlist</HeaderH1>
+        </HeaderContent>
+      </HeaderContainer>
+    </>
   );
 };
 
