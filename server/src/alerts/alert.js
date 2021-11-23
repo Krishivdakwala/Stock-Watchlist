38f163b360aeeb792769fbe73bc727336ffad1b5
Akash-213 <akash213kulkarni@gmail.com>
2021-11-23 11:00:05 +0530

    mails done

--- server/src/alerts/alert.js
+++ server/src/alerts/alert.js
@@ -15,11 +15,24 @@ const welcomeMail = (email, name) => {
     subject: "Welcome OnBoard !!",
     html: `
       <div>
-        <h1>Hello ${name}</h1>
 
-        <h2>Akash Kulkarni</h2>
-
-        <h4> Happy Investing </h4>
+        <h2>Hello ${name}</h2>
+        <h2>Thank you for registering with us!!</h2>
+        <h3>Welcome to stock market</h3>
+        <h4>We would like to tell you about features offered by us.<br><br> Our features :-</h4>
+        <ul>
+            <li><b>Real time data of stocks</b></li>
+            <li><b>You can create multiple watchlists where you can insert various stocks of your choice</b></li>
+            <li><b>Get notified through mail about a stock, when it hits the desired price of yours</b></li>
+        </ul>
+    
+        <h4>We hope that you enjoy our services.
+            <br>
+            <br>Thank You!!
+            <br>
+            <br>Regards,
+            <br>Stock market
+        </h4>
       </div>
     `,
   };
@@ -41,10 +54,14 @@ const newsMail = (email, name) => {
     html: `
       <div>
         <h1>Hello ${name}</h1>
-
-        <h2>Akash Kulkarni</h2>
-
-        <h4> Happy Investing </h4>
+        <h2>Greetings of the day!!</h2>
+        <h4>Don't miss out on latest updates. Catch all the latest activity on the news tab. <br>
+            Have a great day!!
+            <br>
+            <br>Regards,
+            
+            <br>Stock market
+        </h4>
       </div>
     `,
   };
@@ -91,10 +108,16 @@ const alertMail = (email, name) => {
     html: `
       <div>
         <h1>Hello ${name}</h1>
-
-        <h2>Your target was triggered !!</h2>
-
-        <h4> Happy Investing </h4>
+        <h2>Greetings of the day!!</h2>
+       
+        <h4>This is to inform you that XYZ stock has reached the desired value which was 
+            set by you. Please login to your account for further activities.
+            <br>
+            <br>Thank You!!
+            <br>
+            <br>Regards,
+            <br>Stock market
+        </h4>
       </div>
     `,
   };
